a37414dcfe288f7e8b3c0fc4fb6fcc2d2b22e3be
Azat Sibagatulin <sibagatulin.azat@yandex.ru>
2022-12-07 22:40:09 +0300

    Add: offline KnnModel

--- service/reco_models/reco_models.py
+++ service/reco_models/reco_models.py
@@ -1,6 +1,8 @@
 import os
 import pickle
-from typing import Any, List
+from typing import Any, List, Optional
+import numpy
+
 
 cwd = os.path.dirname(__file__)
 
@@ -8,24 +10,6 @@ cwd = os.path.dirname(__file__)
 class simple_popular_model():
 
     def __init__(self):
-        self.dataset = pickle.load(
-            open(
-                os.path.join(
-                    cwd,
-                    'popular_dataset.pickle'
-                ),
-                'rb'
-            )
-        )
-        self.model: Any = pickle.load(
-            open(
-                os.path.join(
-                    cwd,
-                    'popular_model.pickle'
-                ),
-                'rb'
-            )
-        )
         self.top_popular_list: List = pickle.load(
             open(
                 os.path.join(
@@ -41,18 +25,15 @@ class simple_popular_model():
         user_id: int,
         k_recs: int
     ) -> List:
-        # проверяю юзер в датасете или нет
-        if user_id in self.dataset.user_id_map.external_ids:
-            recos = list(
-                self.model.recommend(
-                    [user_id],
-                    dataset=self.dataset,
-                    k=k_recs,
-                    filter_viewed=False
-                )['item_id'].values
-            )
-        else:
-            recos = list(
-                self.top_popular_list[:k_recs]
-            )
-        return recos
+        return list(self.top_popular_list[:k_recs])
+
+
+class KnnModel:
+    def __init__(self, path: str = f"{cwd}/hot_reco_dict.pickle"):
+        with open(path, "rb") as f:
+            self.model = pickle.load(f)
+
+    def predict(self, user_id: int) -> Optional[List[int]]:
+        if user_id in self.model.keys():
+            return self.model[user_id]
+        return None
